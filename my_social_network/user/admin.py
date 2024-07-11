e357eb1ac7979e2ef3cd3ca79312dbf9ac95f064
Dimitrii <find.test.hire@gmail.com>
2024-07-10 22:12:56 -0500

    several issues fixed and ready for auth view

--- my_social_network/user/admin.py
+++ my_social_network/user/admin.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
-  list_display = ['id', 'first_name', 'last_name', 'email']
+  list_display = ['id', 'first_name', 'last_name', 'email', 'is_staff']
 
 admin.site.register(User, CustomUserAdmin)
