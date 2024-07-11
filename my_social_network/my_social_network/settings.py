7b978422ef804bbb7198a7301b3adc5b08384adb
Dimitrii <find.test.hire@gmail.com>
2024-07-10 21:06:33 -0500

    second commit

--- my_social_network/my_social_network/settings.py
+++ my_social_network/my_social_network/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restframework_simplejwt',
+    # 'restframework_simplejwt',
     'rest_framework',
     'djoser',
     "authorization",
--- my_social_network/user/admin.py
+++ my_social_network/user/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 
 # Register your models here.
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
   list_display = ['id', 'first_name', 'last_name', 'email']
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
